424703d35c3ca1daa00c0750c3e5a61d77aabf51
srinu856 <101277128+srinu856@users.noreply.github.com>
2023-07-20 15:47:10 +0530

    Update sample_code.sh
    code commit

--- sample_code.sh
+++ sample_code.sh
@@ -3,6 +3,6 @@
 
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
-echo "This is running in a build step and it shows changes"
+echo "This is running in a build step to jenkins and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
