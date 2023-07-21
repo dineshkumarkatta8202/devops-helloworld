47782aa7b9c143070e74d8053bebb09b6d0fed8a
dineshkumarkatta8202 <137852839+dineshkumarkatta8202@users.noreply.github.com>
2023-07-21 08:46:00 +0530

    Update sample_code.sh
    updating the code.......21/7/2023

--- sample_code.sh
+++ sample_code.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 
-echo "This is an Samples Script from GitHub"
-echo "This is test from Linux Server"
-echo "This is running in a build step to jenkins and it shows changes"
-SUM=$((FIRSTNUMBER + SECONDNUMBER))
+echo "This is a Samples Script from GitHub ///dinesh"
+echo "This is a test from Linux Server"
+echo "This is running in a build step to Jenkins and it shows changes"
+SUM=$((FIRSTNUMBER + SECOND NUMBER))
 echo "$SUM"
